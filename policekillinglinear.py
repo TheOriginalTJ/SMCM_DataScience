8940755e8cd74f655aa0aabd09106e76b1d39250
japellet <japellet@smcm.edu>
2017-04-28 13:45:05 -0400

    PoliceKillings LinReg:%White vs household income
    Uses police killings dataset

--- policekillinglinear.py
+++ policekillinglinear.py
@@ -3,11 +3,18 @@
 Created on Mon Apr 17 13:54:53 2017
 
 @author: JP
-"""
-"""
+Purpose: This code is to generate a linear regression using a police killings data set. The two axis are:
+-Personal household income is X axis
+-Nonewhite percentage is Y axis
 
-Personal household income is X axis
-Nonewhite percentage is Y axis
+Our hypothesis is that the regression will show a trend of low income households having a higher concentration 
+of police killings, as well as an even greater concentration in non-white groups.
+
+
+We can assume that 
+0-39% is non-white
+40-79% is "mixed"
+80-100% is white
 
 USES police_killings.csv"""
 
@@ -18,23 +25,21 @@ from sklearn import linear_model
 
 infile = open('police_killings.csv', 'r')
 
-"Evaluating percent non-white with household income"
-
-
 
 medianhouseincome = []
 percentwhite = []
 
 
 next(infile)
-
+#Iterates through police_killings.csv by row to choose two columns: % White and Median Household Income
 for line in infile:
         row = line.split(",")
         if row[17] == 'NA' or row[14] == 'NA':
             break
         medianhouseincome.append(int(row[17]))
-        percentwhite.append(100-(float(row[14])))
+        percentwhite.append((float(row[14])))
         
+
 xo = medianhouseincome
 yo = percentwhite
 
@@ -58,7 +63,13 @@ print pred
 
 plt.scatter(x, y, color = 'black')
 plt.plot(x_test, regr.predict(x_test), color='red', linewidth=1)
+plt.ylim([-5,105])
+plt.ylabel('% White')
+plt.xlabel('Median Household Income')
+plt.title('Police Killings Linear Regression: % White vs Median Household Income')
+plt.grid(True)
+
 plt.show()
-print "Y axis is percent non-white (0 = white, 100 = nonwhite)"
-print "X axis is median household income"
-        
+
+
+plt.savefig('PoliceKillingsLinear_percentWhiteVShouseholdIncome.png')
